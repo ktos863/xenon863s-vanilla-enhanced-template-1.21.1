a382c0aa8f8d131afc03135a4998c0ca45155767
Xenon863 <maciejewskimechlin123@gmail.com>
2024-10-19 20:14:26 +0200

    Initial commit

---
+++ src/main/java/com/xenon863/vanilla_enhanced/Xenon863sVanillaEnhancedClient.java
@@ -0,0 +1,10 @@
+package com.xenon863.vanilla_enhanced;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Xenon863sVanillaEnhancedClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
---
+++ src/main/java/com/xenon863/vanilla_enhanced/Xenon863sVanillaEnhancedDataGenerator.java
@@ -0,0 +1,11 @@
+package com.xenon863.vanilla_enhanced;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Xenon863sVanillaEnhancedDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
